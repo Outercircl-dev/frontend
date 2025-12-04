4c3befb24e03c59b1b988482aa6032b282152c87
Yash Vardhan <187936763+YashVardhan-21@users.noreply.github.com>
2025-12-04 18:26:44 +0530

    TEST-001: add formatTimeAgo utility function with tests

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  verbose: true,
+};
+
+module.exports = config;
+
