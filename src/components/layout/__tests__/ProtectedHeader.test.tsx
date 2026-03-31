dff0e380c2d6aac1959a0d68bdc087097de5f172
Harsh Gupta <harshrp94@gmail.com>
2026-03-31 20:14:29 +0100

    Fixing tests

--- src/components/layout/__tests__/ProtectedHeader.test.tsx
+++ src/components/layout/__tests__/ProtectedHeader.test.tsx
@@ -1,8 +1,8 @@
-// Copyright (c) 2026 Outer Circle. All rights reserved.
-
 /**
  * @jest-environment jsdom
  */
+
+// Copyright (c) 2026 Outer Circle. All rights reserved.
 
 import { render, screen } from '@testing-library/react'
 
